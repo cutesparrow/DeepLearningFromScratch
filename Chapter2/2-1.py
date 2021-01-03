d5a83e5ed3e26bba720e1a23f71a76a59f7b5052
Ziven <shigaoyu615@gmail.com>
2021-01-03 11:30:33 +1100

    finish chapter2

--- Chapter2/2-1.py
+++ Chapter2/2-1.py
@@ -3,50 +3,59 @@
 #通过叠加层(加深 层)，感知机能进行更加灵活的表示。
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1+x2*w2
+    tmp = x1 * w1 + x2 * w2
     if tmp > theta:
         return 1
     else:
         return 0
-print(AND(0,0))
-print(AND(1,0))
-print(AND(0,1))
-print(AND(1,1))
-import numpy as np
-x = np.array([0,1])
-w = np.array([0.5,0.5])
-b = -0.7
-print(w*x)
-print(np.sum(w*x)+b)
-def AND2(x1,x2):
-    x = np.array([x1,x2])
-    w = np.array([0.5,0.5])
-    b = -0.7
-    tmp = np.sum(x*w)+b
-    if tmp>0:
-        return 1
-    else:
-        return 0
-def NAND(x1,x2):
-    x = np.array([x1,x2])
-    w = np.array([-0.5,-0.5])
-    b = 0.7
-    tmp = np.sum(w*x)+b
-    if tmp<=0:
-        return 0
-    else:
-        return 1
-def OR(x1,x2):
-    x = np.array([x1,x2])
-    w = np.array([-0.5,-0.5])
-    b = 0.2
-    tmp = np.sum(w*x)+b
-    if tmp<0:
-        return 1
-    else:
-        return 0
-def XOR(x1,x2):
-    s1 = NAND(x1,x2)
-    s2 = OR(x1,x2)
-    return AND(s1,s2)
 
+
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
+import numpy as np
+x = np.array([0, 1])
+w = np.array([0.5, 0.5])
+b = -0.7
+print(w * x)
+print(np.sum(w * x) + b)
+
+
+def AND2(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x * w) + b
+    if tmp > 0:
+        return 1
+    else:
+        return 0
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.2
+    tmp = np.sum(w * x) + b
+    if tmp < 0:
+        return 1
+    else:
+        return 0
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)

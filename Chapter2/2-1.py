7919fd09d5ae2797546ed0bc936fe519b79cc062
Ziven <shigaoyu615@gmail.com>
2021-01-03 11:14:55 +1100

    finish chapter2

---
+++ Chapter2/2-1.py
@@ -0,0 +1,52 @@
+#感知机的局限性就在于它只能表示由一条直线分割的空间
+#通过 简单的 NAND 元件就可以实现计算机这样复杂的系统。
+#通过叠加层(加深 层)，感知机能进行更加灵活的表示。
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1+x2*w2
+    if tmp > theta:
+        return 1
+    else:
+        return 0
+print(AND(0,0))
+print(AND(1,0))
+print(AND(0,1))
+print(AND(1,1))
+import numpy as np
+x = np.array([0,1])
+w = np.array([0.5,0.5])
+b = -0.7
+print(w*x)
+print(np.sum(w*x)+b)
+def AND2(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([0.5,0.5])
+    b = -0.7
+    tmp = np.sum(x*w)+b
+    if tmp>0:
+        return 1
+    else:
+        return 0
+def NAND(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([-0.5,-0.5])
+    b = 0.7
+    tmp = np.sum(w*x)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
+def OR(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([-0.5,-0.5])
+    b = 0.2
+    tmp = np.sum(w*x)+b
+    if tmp<0:
+        return 1
+    else:
+        return 0
+def XOR(x1,x2):
+    s1 = NAND(x1,x2)
+    s2 = OR(x1,x2)
+    return AND(s1,s2)
+
